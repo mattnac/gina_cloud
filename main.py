1b4398c216660ec0f5e4aeef8afc3466a683a079
Mattias Dahlgren <mattias.dahlgren@leovegas.com>
2022-11-14 12:07:25 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,98 @@
+import click, zipfile
+from colorlog import ColoredFormatter
+import logging
+from logging.config import dictConfig
+from os import listdir
+from os.path import isfile, join
+from pprint import pprint
+import tail, re
+from xml.etree import ElementTree
+
+TRIGGERS = list()
+
+
+def merge(xml_list):
+    xml_data = None
+    for xml_string in xml_list:
+        data = ElementTree.fromstring(xml_string)
+        if xml_data is None:
+            xml_data = data
+        else:
+            xml_data.extend(data)
+    if xml_data is not None:
+        return xml_data.findall(".//Trigger")
+
+
+def configure_logging(verbose):
+    log_level = 50 - ((1 + verbose) * 10)
+    logging.root.setLevel(log_level)
+    log_format = "  %(log_color)s%(levelname)-8s%(reset)s | %(log_color)s%(message)s%(reset)s"
+    formatter = ColoredFormatter(log_format)
+
+    stream = logging.StreamHandler()
+    stream.setLevel(log_level)
+    stream.setFormatter(formatter)
+
+    logging.root.setLevel(log_level)
+    logging.root.addHandler(stream)
+
+
+@click.group()
+@click.option("--triggers-dir", "-T", help="Directory with gina triggers",
+              type=click.Path(dir_okay=True, file_okay=False, resolve_path=True), default="triggers", show_default=True)
+@click.option('--log-file', '-L', default="~/shared/eqlogs/eqlog_Karolus_P1999Green.txt", help="Log file",
+              required=True,
+              show_default=True, type=click.Path(dir_okay=False, file_okay=True, resolve_path=True))
+@click.option("--verbose", "-v", count=True, default=(logging.WARNING / 20),
+              help="controls log level")
+@click.pass_context
+def cli(ctx, triggers_dir, log_file, verbose):
+    configure_logging(verbose)
+    ctx.ensure_object(dict)
+    global TRIGGERS
+    TRIGGERS = get_triggers(triggers_dir)
+    ctx.obj["log_file"] = log_file
+
+
+
+def get_triggers(dir):
+    triggers = list()
+    files = get_file_list(dir)
+    xml_data = list()
+    for f in files:
+        with zipfile.ZipFile(f, 'r') as zip_ref:
+            with zip_ref.open('ShareData.xml') as xml_file:
+                xml_data.append(xml_file.read())
+
+    for trigger in merge(xml_data):
+        triggers.append(dict(
+            name=trigger.find('./Name').text,
+            regex=trigger.find('./TriggerText').text
+        ))
+
+    return triggers
+
+
+def get_file_list(dir):
+    return [
+        "{}/{}".format(dir, f) for f in listdir(dir) if isfile(join(dir, f))
+    ]
+
+
+@cli.command("start")
+@click.pass_context
+def start(ctx):
+    t = tail.Tail(ctx.obj["log_file"])
+    t.register_callback(check_pattern)
+    t.follow(s=0.1)
+
+
+def check_pattern(line):
+    click.echo(line, nl=False)
+    for p in TRIGGERS:
+        if re.match(p["regex"].replace("?<", "?P<"), line):
+            click.echo("MATCH {}".format(p["name"]))
+
+
+if __name__ == '__main__':
+    cli()
